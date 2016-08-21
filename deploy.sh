8cddce08ecf83862d1e61584d1239b7423c1a454
spiperac <spiperac@denkei.org>
2016-08-21 10:47:42 +0200

    deploy and get updated

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp ./xorg.conf /etc/X11/xorg.conf
+cp ./loader.conf /boot/loader.conf
+cp ./rc.conf /etc/rc.conf
---
+++ get.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp /etc/rc.conf ./
+cp /boot/loader.conf ./
+cp /etc/X11/xorg.conf ./
